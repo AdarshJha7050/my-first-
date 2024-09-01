a4e21beca585a9cb4a9da1cd9989d932b35921db
AdarshJha7050 <jhaa63653@gmail.com>
2024-09-01 15:05:58 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,33 @@
+let num1 = 160;
+let num2 = 120;
+
+    if(num1 > num2)
+        console.log(num1, " is big");
+        
+    else if(num2 > num1)
+    console.log(num2, "is big" );
+else
+console.log("both are same");
+
+// write a program that calculates based on the bat amount using If Else.//
+let amount = 5000;
+
+if(amount>0 ){
+    console.log("discount= 50%");
+    amount = amount - (amount*50)/100;
+    console.log(amount);
+    
+}
+   
+
+//loop//
+for (let a =20; a <50; a++)
+{
+    console.log(a);
+    
+}
+
+
+
+    
+    
